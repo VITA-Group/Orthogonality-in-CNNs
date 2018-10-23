66b7e7bd9728103c9f4b41d8b2ba4e5a9e6d892d
Nitin Kumar Bansal <bansalcec@gmail.com>
2018-10-22 19:11:40 -0500

    Add files via upload

--- SVHN/train.py
+++ SVHN/train.py
@@ -1,1 +1,303 @@
-Hello World!
+# run train.py --dataset cifar10 --model resnet18 --data_augmentation --cutout --length 16
+# run train.py --dataset cifar100 --model resnet18 --data_augmentation --cutout --length 8
+# run train.py --dataset svhn --model wideresnet --learning_rate 0.01 --epochs 160 --cutout --length 20
+
+import pdb
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import shutil
+import os
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import MultiStepLR
+
+from torchvision.utils import make_grid
+from torchvision import datasets, transforms
+
+from util.misc import CSVLogger
+from util.cutout import Cutout
+
+from model.resnet import ResNet18
+from model.wide_resnet import WideResNet
+from torch.nn.functional import normalize
+
+model_options = ['resnet18', 'wideresnet']
+dataset_options = ['cifar10', 'cifar100', 'svhn']
+
+parser = argparse.ArgumentParser(description='CNN')
+parser.add_argument('--dataset', '-d', default='cifar10',
+                    choices=dataset_options)
+parser.add_argument('--model', '-a', default='resnet18',
+                    choices=model_options)
+parser.add_argument('--batch_size', type=int, default=256,
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='number of epochs to train (default: 20)')
+parser.add_argument('--learning_rate', type=float, default=0.1,
+                    help='learning rate')
+parser.add_argument('--data_augmentation', action='store_true', default=False,
+                    help='augment data by flipping and cropping')
+#parser.add_argument('--cutout', action='store_true', default=False,
+#                    help='apply cutout')
+parser.add_argument('--n_holes', type=int, default=1,
+                    help='number of holes to cut out from image')
+parser.add_argument('--length', type=int, default=16,
+                    help='length of the holes')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=0,
+                    help='random seed (default: 1)')
+parser.add_argument('--ortho-decay', '--od', default=1e-2, type=float,
+                    help = 'ortho weight decay')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+cudnn.benchmark = True  # Should make training should go faster for large models
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+
+
+"""Function used for Orthogonal Regularization"""
+
+def l2_reg_ortho(mdl):
+        l2_reg = None
+        for W in mdl.parameters():
+                if W.ndimension() < 2:
+                        continue
+                else:
+                        cols = W[0].numel()
+                        rows = W.shape[0]
+                        w1 = W.view(-1,cols)
+                        wt = torch.transpose(w1,0,1)
+                        m  = torch.matmul(wt,w1)
+                        ident = Variable(torch.eye(cols,cols))
+                        ident = ident.cuda()
+
+                        w_tmp = (m - ident)
+                        height = w_tmp.size(0)
+                        u = normalize(w_tmp.new_empty(height).normal_(0, 1), dim=0, eps=1e-12)
+                        v = normalize(torch.matmul(w_tmp.t(), u), dim=0, eps=1e-12)
+                        u = normalize(torch.matmul(w_tmp, v), dim=0, eps=1e-12)
+                        sigma = torch.dot(u, torch.matmul(w_tmp, v))
+
+                        if l2_reg is None:
+                                l2_reg = (torch.norm(sigma,2))**2
+                        else:
+                                l2_reg = l2_reg + (torch.norm(sigma,2))**2
+        return l2_reg
+
+test_id = args.dataset + '_' + args.model
+
+print(args)
+
+# Image Preprocessing
+if args.dataset == 'svhn':
+    normaliz = transforms.Normalize(mean=[x / 255.0 for x in[109.9, 109.7, 113.8]],
+                                     std=[x / 255.0 for x in [50.1, 50.6, 50.8]])
+else:
+    normaliz = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                     std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
+
+train_transform = transforms.Compose([])
+if args.data_augmentation:
+    train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
+    train_transform.transforms.append(transforms.RandomHorizontalFlip())
+train_transform.transforms.append(transforms.ToTensor())
+train_transform.transforms.append(normaliz)
+#if args.cutout:
+#    train_transform.transforms.append(Cutout(n_holes=args.n_holes, length=args.length))
+
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    normaliz])
+
+if args.dataset == 'cifar10':
+    num_classes = 10
+    train_dataset = datasets.CIFAR10(root='data/',
+                                     train=True,
+                                     transform=train_transform,
+                                     download=True)
+
+    test_dataset = datasets.CIFAR10(root='data/',
+                                    train=False,
+                                    transform=test_transform,
+                                    download=True)
+elif args.dataset == 'cifar100':
+    num_classes = 100
+    train_dataset = datasets.CIFAR100(root='data/',
+                                      train=True,
+                                      transform=train_transform,
+                                      download=True)
+
+    test_dataset = datasets.CIFAR100(root='data/',
+                                     train=False,
+                                     transform=test_transform,
+                                     download=True)
+elif args.dataset == 'svhn':
+    num_classes = 10
+    train_dataset = datasets.SVHN(root='data/',
+                                  split='train',
+                                  transform=train_transform,
+                                  download=True)
+
+    extra_dataset = datasets.SVHN(root='data/',
+                                  split='extra',
+                                  transform=train_transform,
+                                  download=True)
+
+    # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
+    data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
+    labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
+    train_dataset.data = data
+    train_dataset.labels = labels
+
+    test_dataset = datasets.SVHN(root='data/',
+                                 split='test',
+                                 transform=test_transform,
+                                 download=True)
+
+# Data Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=args.batch_size,
+                                           shuffle=True,
+                                           pin_memory=True,
+                                           num_workers=2)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=args.batch_size,
+                                          shuffle=False,
+                                          pin_memory=True,
+                                          num_workers=2)
+
+if args.model == 'resnet18':
+    cnn = ResNet18(num_classes=num_classes)
+elif args.model == 'wideresnet':
+    if args.dataset == 'svhn':
+        cnn = WideResNet(depth=16, num_classes=num_classes, widen_factor=8,
+                         dropRate=0.4)
+    else:
+        cnn = WideResNet(depth=28, num_classes=num_classes, widen_factor=10,
+                         dropRate=0.3)
+
+#cnn = torch.nn.DataParallel(cnn).cuda()
+cnn = cnn.cuda()
+criterion = nn.CrossEntropyLoss().cuda()
+cnn_optimizer = torch.optim.SGD(cnn.parameters(), lr=args.learning_rate,
+                                momentum=0.9, nesterov=True, weight_decay=5e-4)
+odecay = args.ortho_decay
+
+if args.dataset == 'svhn':
+    scheduler = MultiStepLR(cnn_optimizer, milestones=[80, 120], gamma=0.1)
+else:
+    scheduler = MultiStepLR(cnn_optimizer, milestones=[60, 120, 160], gamma=0.2)
+
+filename = 'logs/' + test_id + '.csv'
+csv_logger = CSVLogger(args=args, fieldnames=['epoch', 'train_acc', 'test_acc'], filename=filename)
+
+
+def test(loader):
+    cnn.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
+    correct = 0.
+    total = 0.
+    for images, labels in loader:
+        images = images.cuda()
+        labels = labels.cuda()
+
+        with torch.no_grad():
+            pred = cnn(images)
+
+        pred = torch.max(pred.data, 1)[1]
+        total += labels.size(0)
+        correct += (pred == labels).sum().item()
+
+    val_acc = correct / total
+    cnn.train()
+    return val_acc
+
+def adjust_ortho_decay_rate(epoch):
+    o_d = args.ortho_decay
+
+    if epoch > 120:
+       o_d = 0.0
+    elif epoch > 70:
+       o_d = 1e-6 * o_d
+    elif epoch > 50:
+       o_d = 1e-4 * o_d
+    elif epoch > 20:
+       o_d = 1e-3 * o_d
+
+    return o_d
+
+max_accuracy = 0
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    """Saves checkpoint to disk"""
+    directory = "checkpoint/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = directory + filename
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'checkpoint/'+ 'model_best.pth.tar')
+
+for epoch in range(args.epochs):
+
+    xentropy_loss_avg = 0.
+    correct = 0.
+    total = 0.
+    odecay = adjust_ortho_decay_rate(epoch+1)
+    progress_bar = tqdm(train_loader)
+    for i, (images, labels) in enumerate(progress_bar):
+        progress_bar.set_description('Epoch ' + str(epoch))
+
+        images = images.cuda()
+        labels = labels.cuda()
+
+        cnn.zero_grad()
+        pred = cnn(images)
+
+        xentropy_loss = criterion(pred, labels)
+        oloss = odecay * l2_reg_ortho(cnn)
+        xentropy_loss = xentropy_loss + oloss
+        xentropy_loss.backward()
+        cnn_optimizer.step()
+
+        xentropy_loss_avg += xentropy_loss.item()
+
+        # Calculate running average of accuracy
+        pred = torch.max(pred.data, 1)[1]
+        total += labels.size(0)
+        correct += (pred == labels.data).sum().item()
+        accuracy = correct / total
+
+        progress_bar.set_postfix(
+            xentropy='%.3f' % (xentropy_loss_avg / (i + 1)),
+            acc='%.3f' % accuracy)
+
+    test_acc = test(test_loader)
+    tqdm.write('test_acc: %.3f' % (test_acc))
+
+    scheduler.step(epoch)
+
+    row = {'epoch': str(epoch), 'train_acc': str(accuracy), 'test_acc': str(test_acc)}
+    csv_logger.writerow(row)
+    #Saving checkpoint
+    is_best = test_acc > max_accuracy
+    max_accuracy = max(test_acc, max_accuracy)
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'state_dict': cnn.state_dict(),
+        'best_prec1': max_accuracy,
+    }, is_best)
+
+print ("Best Accuracy", max_accuracy)
+#torch.save(cnn.state_dict(), 'checkpoints/' + test_id + '.pt')
+csv_logger.close()
